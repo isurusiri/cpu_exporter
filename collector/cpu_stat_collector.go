2f75704a50f67d3f73b838eceb0769265e8574d6
isurusiri <isuruanatomy@gmail.com>
2020-05-31 16:23:59 +0530

    initial implementation of collector

--- collector/cpu_stat_collector.go
+++ collector/cpu_stat_collector.go
@@ -1,1 +1,28 @@
 package cpustatcollector
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+// CPUMetrics defines the CPU metrics going to expose from
+// this exporter.
+type CPUMetrics struct {
+	cupIdle        prometheus.Gauge
+	cpuTotal       prometheus.Gauge
+	cpuUtilization prometheus.Gauge
+}
+
+// New craetes a new CPU metrics instance
+func New() {
+	// craete a new CPU metrics instance
+}
+
+// Init initializes the metrics scraping
+func Init() {
+	// initialize the metrics scraping
+}
+
+// Collect collects metrics from the client periodically
+func Collect() {
+	// collects metrics from the client periodically
+}
