703bf280fcd0b40866bb7e615cc3c70cbf4da9e9
isurusiri <isuruanatomy@gmail.com>
2020-06-02 05:13:12 +0530

    collector implementation

--- collector/cpustatcollector.go
+++ collector/cpustatcollector.go
@@ -12,13 +12,27 @@ type CPUMetrics struct {
 	cpuUtilization prometheus.Gauge
 }
 
+// CPUCollector represents CPU Collector object
+type CPUCollector struct {
+	cpuMetrics CPUMetrics
+	// cpuClient  cpu_client.CPUStat
+}
+
 // New craetes a new CPU metrics instance
 func New() *CPUMetrics {
-	// craete a new CPU metrics instance
 	return &CPUMetrics{
-		cupIdle:        prometheus.NewGauge(),
-		cpuTotal:       prometheus.NewGauge(),
-		cpuUtilization: prometheus.NewGauge(),
+		cupIdle: prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: "cpu_exporter_cpu_idle_time",
+			Help: "Shows the CPU idle time",
+		}),
+		cpuTotal: prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: "cpu_exporter_cpu_total_usage",
+			Help: "Shows the total CPU availability",
+		}),
+		cpuUtilization: prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: "cpu_exporter_cpu_utilization",
+			Help: "Shows the total CPU utilization",
+		}),
 	}
 }
 
